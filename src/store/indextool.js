89b75c631a77d7aedf09ade77cd81cd8c6efdd0b
Qubit1010 <hassanaleem86@gmail.com>
2022-08-17 14:46:11 +0500

    Project Initialize

---
+++ src/store/indextool.js
@@ -0,0 +1,65 @@
+// import { legacy_createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+export const INCREMENT = 'increment'
+
+const initialState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state){
+      state.counter++;
+    },
+    decrement(state){
+      state.counter--;
+    },
+    increase(state, action){
+      state.counter = state.counter + action.amount
+    },
+    toggleCounter(state){
+      state.showCounter = !state.showCounter;
+    },
+  }
+})
+
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === INCREMENT) {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+ 
+//   if (action.type === "toggle") {
+//     return {
+//       showCounter: !state.showCounter,
+//       counter: state.counter,
+//     };
+//   }
+
+//   return state;
+// };
+
+// const store = legacy_createStore(counterReducer);
+const storeViaToolkit = configureStore({
+  reducer: counterSlice.reducer
+})
+
+// export default store;
+export default storeViaToolkit;
